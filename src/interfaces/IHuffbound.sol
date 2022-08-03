b01d8024bce107d8a794ee8588bdbd28a30244f8
PraneshASP <praneshas2000@gmail.com>
2022-08-04 01:43:25 +0530

    ♻️ Refactor

---
+++ src/interfaces/IHuffbound.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+interface Huffbound {
+    function name() external view returns (bytes32);
+
+    function symbol() external view returns (bytes32);
+
+    function tokenURI(uint256) external view returns (bytes32);
+
+    function owner() external view returns (address);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function isApprovedForAll(address, address) external view returns (bool);
+
+    function mint(address, uint256) external;
+
+    function transfer(address, uint256) external;
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external;
+
+    function approve(address, uint256) external;
+
+    function setApprovalForAll(address, bool) external;
+
+    function getApproved(uint256) external view returns (address);
+}
--- test/Soulbound.t.sol
+++ test/Soulbound.t.sol
@@ -5,35 +5,8 @@ import "foundry-huff/HuffDeployer.sol";
 import "foundry-huff/HuffConfig.sol";
 import "forge-std/Test.sol";
 
-interface Huffbound {
-    function name() external view returns (bytes32);
-
-    function symbol() external view returns (bytes32);
-
-    function tokenURI(uint256) external view returns (bytes32);
-
-    function owner() external view returns (address);
-
-    function balanceOf(address) external view returns (uint256);
-
-    function isApprovedForAll(address, address) external view returns (bool);
-
-    function mint(address, uint256) external;
-
-    function transfer(address, uint256) external;
-
-    function transferFrom(
-        address,
-        address,
-        uint256
-    ) external;
-
-    function approve(address, uint256) external;
-
-    function setApprovalForAll(address, bool) external;
-
-    function getApproved(uint256) external view returns (address);
-}
+// Internal imports
+import {Huffbound} from "../src/interfaces/IHuffbound.sol";
 
 contract HuffboundTest is Test {
     /// @dev System under test
